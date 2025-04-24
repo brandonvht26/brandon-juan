ec204cef943a2186a4b6bad2f7eec78a52db6e52
brandonvht26 <brandon.v.huera.t@gmail.com>
2025-04-24 09:24:11 -0500

    Main modificado

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,33 @@
 //2 métodos, información del vehiculo, y frenar
 //crear 3 objetos
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello wordl!");
+        Scanner sc = new Scanner(System.in); // Definir objeto Scanner
+
+        System.out.print("\n--- Bienvenido al sistema de ingreso de coches --- \n");
+
+        // Registro de objetos
+
+        System.out.print("\nIngrese la marca del vehiculo: ");
+        String marca = sc.nextLine();
+        System.out.print("Ingrese el modelo del vehiculo: ");
+        String placa = sc.nextLine();
+        System.out.print("Ingrese la placa del vehiculo: ");
+        String modelo = sc.nextLine();
+        System.out.print("Ingrese el valor del vehiculo: ");
+        double valor = sc.nextDouble();
+        System.out.print("\n---------------- ¡Registro exitoso! ----------------\n");
+
+        car.Carro(marca, precio, modelo, placa);
+
+        // Metodos
+
+        car.mostrarInformacion();
+
+        sc.close();
     }
 }
